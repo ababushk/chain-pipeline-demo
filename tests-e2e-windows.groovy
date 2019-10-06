716993fead44e7176e925a025e4639fb1ec013f7
Andrey Babushkin <oxygenxo@gmail.com>
2019-10-06 16:56:04 +0300

    add trigger for e2e-windows

--- tests-e2e-windows.groovy
+++ tests-e2e-windows.groovy
@@ -1,1 +1,14 @@
 #!groovy
+properties(
+    [
+        pipelineTriggers([
+            upstream(upstreamProjects: "../multibranch-build-windows/${BRANCH_NAME}",
+                     threshold: hudson.model.Result.SUCCESS)
+        ])
+    ]
+)
+
+
+currentBuild.upstreamBuilds?.each { b ->
+    echo b.getFullProjectName()
+}
